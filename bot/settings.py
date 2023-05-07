5a4f271eb49798ccda6f22c33a506522b81cd36d
19taurus79 <19taurus79@gmail.com>
2023-05-07 15:38:23 +0300

    Add settins file

---
+++ bot/settings.py
@@ -0,0 +1,11 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+try:
+    BOT_TOKEN =os.environ['BOT_TOKEN']
+except KeyError as err:
+    logging.critical(f'Can`t read token from environment variable. Message: {err}')
+    raise KeyError(err)
