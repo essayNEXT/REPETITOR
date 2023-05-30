31c5ae84aef6833265b5056a9c9ca6509ae9b214
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-05-30 18:53:43 +0300

    розширено оголошенням команд в меню та додано новий хендлер

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,11 +1,14 @@
 import asyncio
 import logging
-from handlers import echo
+from handlers import echo, inline_kb_test
+from utils.commands import set_commands
 from create_bot import bot, dp
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
+    await set_commands(bot)
+    dp.include_router(inline_kb_test.router)
     dp.include_router(echo.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=True)
