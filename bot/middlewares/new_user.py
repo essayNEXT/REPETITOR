b691249e7ff05469ede9b6b0efc0c9bfd08f6ae3
19taurus79 <19taurus79@gmail.com>
2023-05-31 15:56:12 +0300

    fixed bugs (black)

--- bot/middlewares/new_user.py
+++ bot/middlewares/new_user.py
@@ -3,28 +3,29 @@ from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, Message
 
 # Для проверки работы кода (начало)
-base = [548019148,]
+base = [
+    548019148,
+]
+
+
 def is_new_user(message: Message) -> bool:
     return message.from_user.id in base
 
+
 def create_new_user():
     return "Новый пользователь"
+
 
 # Для проверки работы кода (конец)
 
 
 class NewUserMiddleware(BaseMiddleware):
     async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str,Any],
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any],
     ) -> Any:
         if is_new_user(event):
             return await handler(event, data)
-        await event.answer(f"{create_new_user()}") # Вставить функцию создания пользователя
-
-
-
-
-
+        await event.answer(f"{create_new_user()}")  # Вставить функцию создания пользователя
