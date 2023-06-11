54a024ba24bcf305d7b09bbb4877c4ffb36225f1
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-06-11 17:13:56 +0200

    added: google translate func with async wrapper

---
+++ backend/repetitor_backend/external_api/google.py
@@ -0,0 +1,27 @@
+import asyncio
+from google.cloud import translate_v2 as translate
+
+translate_client = translate.Client()
+
+
+def translate_text(target: str, text: str) -> dict:
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+    return result
+
+
+async def translate(target: str, text: str) -> dict:
+    """Translate 'text' to language 'target' (ISO 639-1).
+    
+    """
+    result = await asyncio.to_thread(translate_text, target, text)
+    return result
---
+++ backend/tests/test_google_translate.py
@@ -0,0 +1,15 @@
+"""
+Test for google translate function.
+"""
+
+import asyncio
+from repetitor_backend.external_api.google import translate
+
+
+async def test_translate() -> None:
+    # simply test: source 
+    result = await translate(target="EN", text="додати")
+    print(result)
+
+
+asyncio.run(test_translate())
