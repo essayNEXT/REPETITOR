b22127c49d2c6e57f1ff089a5e89bd3e634a2bf9
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-06-22 16:30:23 +0300

    fix middleware

--- bot/middlewares/new_user.py
+++ bot/middlewares/new_user.py
@@ -9,16 +9,16 @@ async def get_user_id(message: Message) -> int:
     return user_id
 
 
-async def is_new_user(user_id) -> bool:
+async def is_existing_user(tlg_user_id) -> bool:
     async with aiohttp.ClientSession() as session:
         async with session.get(
-            f"http://repetitor_backend/api/v1/customer/{user_id}"
+            f"http://repetitor_backend/api/v1/customer/{tlg_user_id}"
         ) as response:
             val = await response.json()
             for item in val:
-                val_id = item["id"]
+                val_id = item["tlg_user_id"]
                 val_active = item["is_active"]
-                if val_id == user_id and val_active is True:
+                if val_id == tlg_user_id and val_active is True:
                     return True
                 else:
                     return False
@@ -32,6 +32,8 @@ class NewUserMiddleware(BaseMiddleware):
         data: Dict[str, Any],
     ) -> Any:
         user_id = await get_user_id(message=event)
-        if await is_new_user(user_id):
+        if await is_existing_user(user_id):
             return await handler(event, data)
-        await event.answer(f"Новый пользователь {user_id} {await is_new_user(user_id)}")
+        await event.answer(
+            f"Новый пользователь {user_id} {await is_existing_user(user_id)}"
+        )
