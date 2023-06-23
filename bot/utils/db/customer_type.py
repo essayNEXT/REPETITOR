906cbcf23b1516445abf47dc870921769cb62ddb
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-06-23 15:22:56 +0300

    Create customer_type.py
    add get customer type operation
    
    Co-Authored-By: Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>

---
+++ bot/utils/db/customer_type.py
@@ -0,0 +1,13 @@
+import aiohttp
+
+
+async def get_customer_type(customer_type_name: str) -> dict | None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"http://repetitor_backend/api/v1/type/customer/"
+        ) as response:
+            customer_types = await response.json()
+            for customer_type in customer_types:
+                if customer_type["name"] == customer_type_name:
+                    return customer_type
+        return None
