17d12499a185e722c2fcbd9453ab50d3463e3fa1
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-06-23 15:25:31 +0300

    modify main bot file
    Co-Authored-By: Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,15 +1,16 @@
 import asyncio
 import logging
-from middlewares import new_user
-from handlers import echo, inline_kb_test
+from middlewares import first_contact
+from handlers import echo, inline_kb_test, first_contact_handler
 from utils.commands import set_commands
 from create_bot import bot, dp
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    dp.message.outer_middleware(new_user.NewUserMiddleware())
+    dp.message.outer_middleware(first_contact.FirstContactMiddleware())
     await set_commands(bot)
+    dp.include_router(first_contact_handler.router)
     dp.include_router(inline_kb_test.router)
     dp.include_router(echo.router)
     await bot.delete_webhook(drop_pending_updates=True)
