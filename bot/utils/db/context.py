e372a75612401a9842efc663df1047e60dbcee5f
19taurus79 <19taurus79@gmail.com>
2023-06-28 11:08:17 +0300

    add function get_context (bot)

---
+++ bot/utils/db/context.py
@@ -0,0 +1,10 @@
+from uuid import UUID
+
+import aiohttp
+
+
+async def get_context():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://repetitor_backend/api/v1/context/") as response:
+            context = await response.json()
+            return context
