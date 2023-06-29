abe4e665d77f577297e71d8b26b03056e36adbbf
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-06-29 23:26:11 +0300

    comment unexisting imports

--- backend/repetitor_backend/api/v1/__init__.py
+++ backend/repetitor_backend/api/v1/__init__.py
@@ -8,7 +8,8 @@ from repetitor_backend.api.v1.context.views import router as context_router
 from repetitor_backend.api.v1.customer_context.views import (
     router as customer_context_router,
 )
-from repetitor_backend.api.v1.translate.views import router as translate_router
+
+# from repetitor_backend.api.v1.translate.views import router as translate_router
 
 router = APIRouter()
 
@@ -17,4 +18,4 @@ router.include_router(type_router, tags=["Type"])
 router.include_router(item_router, tags=["Item"])
 router.include_router(context_router, tags=["Context"])
 router.include_router(customer_context_router, tags=["Customer context"])
-router.include_router(translate_router, tags=["Translate"])
+# router.include_router(translate_router, tags=["Translate"])
