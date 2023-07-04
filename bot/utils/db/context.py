5e578b7a3bb82a8bc98a8402cbd0dd74de2ff4fa
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-07-04 21:48:20 +0300

    add get_context_by_short_name function

--- bot/utils/db/context.py
+++ bot/utils/db/context.py
@@ -6,3 +6,12 @@ async def get_context():
         async with session.get("http://repetitor_backend/api/v1/context/") as response:
             context = await response.json()
             return context
+
+
+async def get_context_by_short_name(short_name: str):
+    async with aiohttp.ClientSession() as session:
+        url = "http://repetitor_backend/api/v1/context/"
+        params = {"name_short": short_name}
+        async with session.get(url, params=params) as response:
+            context = await response.json()
+            return context
