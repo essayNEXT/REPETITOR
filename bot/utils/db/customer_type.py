9b58a863d165ea4e0732b4f0e308d08a1c75a36d
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-07-04 21:50:05 +0300

    update get_customer_type

--- bot/utils/db/customer_type.py
+++ bot/utils/db/customer_type.py
@@ -3,11 +3,8 @@ import aiohttp
 
 async def get_customer_type(customer_type_name: str) -> dict | None:
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "http://repetitor_backend/api/v1/type/customer/"
-        ) as response:
+        url = "http://repetitor_backend/api/v1/type/customer/"
+        params = {"name": customer_type_name}
+        async with session.get(url, params=params) as response:
             customer_types = await response.json()
-            for customer_type in customer_types:
-                if customer_type["name"] == customer_type_name:
-                    return customer_type
-        return None
+            return customer_types[0]
