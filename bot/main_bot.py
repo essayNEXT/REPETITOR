66f74bf2156a3a6a8172dcdbe21b6075b83a1c03
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-07-04 21:46:46 +0300

    add customer_context_handler

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,7 +1,12 @@
 import asyncio
 import logging
 from middlewares import first_contact
-from handlers import echo, inline_kb_test, first_contact_handler
+from handlers import (
+    echo,
+    inline_kb_test,
+    first_contact_handler,
+    customer_context_handler,
+)
 from utils.commands import set_commands
 from create_bot import bot, dp
 
@@ -11,6 +16,7 @@ async def main():
     dp.message.outer_middleware(first_contact.FirstContactMiddleware())
     await set_commands(bot)
     dp.include_router(first_contact_handler.router)
+    dp.include_router(customer_context_handler.router)
     dp.include_router(inline_kb_test.router)
     dp.include_router(echo.router)
     await bot.delete_webhook(drop_pending_updates=True)
