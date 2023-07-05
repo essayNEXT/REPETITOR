909ae60eb699ea985e5d6e184128786f472c17d6
Ihor Yakunin <245390@gmail.com>
2023-07-05 14:08:13 +0300

    fix autotranslate Microsoft - return (text, trg_lng)

--- backend/repetitor_backend/external_api/microsoft.py
+++ backend/repetitor_backend/external_api/microsoft.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 import time
-# from aiohttp import ClientSession
-# from repetitor_backend.app import app
+from aiohttp import ClientSession
+
 
 load_dotenv()
 URL = "https://api.cognitive.microsofttranslator.com/translate"
@@ -12,32 +12,33 @@ LOCATION = os.environ.get("LOCATION")
 
 
 async def translate(
-    session,
+    session: ClientSession = None,
     source_lng: str = 'en',
     target_lng: str = 'uk',
     text: str = 'add',
     url: str = URL,
     api_key: str = API_KEY,
     location: str = LOCATION,
-) -> str:
+) -> tuple:
     """
-    The function returns the translation of the entered text, in addition,
-    it compares the resulting translation with:
-    1. by reverse translation or
-    2. with the translation "auto-detecting input language"
-        (additional option, not used yet)
-       and decides on the correctness of the translation.
+    The function performs automatic translation of the word in the specified context
+    of the languages, the order of the languages does not matter
     Used Microsoft Azure Cognitive Services Translator REST APIs
 
     :param session: aiohttp.ClientSession
-    :param source_lng: language from which the translation is carried out
-    :param target_lng: language into which the translation is carried out
+    :param source_lng: first language
+    :param target_lng: second language
     :param text: text to be translated
     :param url: address to request a text translation
     :param api_key: access key
     :param location: site location for the request
-    :return: if the translation is correct, then returns the translation of the input text
+    :return: tuple(result, target language) - if the translation is correct,
+             then returns the translation of the input text
     """
+    from repetitor_backend.app import app
+
+    if not session:
+        session = app.session
 
     params = {"api-version": "3.0", "from": source_lng, "to": [target_lng]}
     params_reverse = {"api-version": "3.0", "from": target_lng, "to": [source_lng]}
@@ -52,38 +53,45 @@ async def translate(
 
     # You can pass more than one object in body.
     body = [{"text": text.lower()}]
-    print('text = ', text.lower())
+    # print('text = ', text.lower())
     async with session.post(url, params=params, headers=headers, json=body) as response:
         response_data = await response.json()
         response_data = source_lng, response_data[0]["translations"][0]["text"]
         res = response_data[1]
-        print('res = ', res)
+        # print('res = ', res)
 
     async with session.post(url, params=params_reverse, headers=headers, json=body) as response:
         response_data = await response.json()
         response_data = source_lng, response_data[0]["translations"][0]["text"]
         res_rev2 = response_data[1]
-        print('res_rev2 = ', res_rev2)
+        # print('res_rev2 = ', res_rev2)
 
     # We perform a reverse translation
     time.sleep(1)
     body_rev = [{"text": res}]
-    async with session.post(
-        url, params=params_reverse, headers=headers, json=body_rev
-    ) as response:
-        response_data = await response.json()
-        response_data = source_lng, response_data[0]["translations"][0]["text"]
-        res_rev = response_data[1]
-        print('res_rev = ', res_rev)
+    if res.lower() != text:
+        async with session.post(
+            url, params=params_reverse, headers=headers, json=body_rev
+        ) as response:
+            response_data = await response.json()
+            response_data = source_lng, response_data[0]["translations"][0]["text"]
+            res_rev = response_data[1]
+            # print('res_rev = ', res_rev)
+        if text.lower() == res_rev.lower():
+            return res, target_lng
 
     if res_rev2.lower() != text:
         async with session.post(url, params=params, headers=headers, json=body) as response:
             response_data = await response.json()
             response_data = source_lng, response_data[0]["translations"][0]["text"]
             res2 = response_data[1]
-            print('res2 = ', res2)
+            # print('res2 = ', res2)
 
-    return res if text.lower() == res_rev.lower() else res_rev2
+        if text.lower() == res2.lower():
+            return res_rev2, source_lng
+        else:
+            "Translation error"
+
 
 
 async def translate_lng(
