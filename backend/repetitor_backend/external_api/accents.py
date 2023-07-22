afe47fd1bf2b3e1e13e9573cbabf071cc202fdce
Ihor Yakunin <245390@gmail.com>
2023-07-22 16:34:08 +0300

    fix "іїйё"

--- backend/repetitor_backend/external_api/accents.py
+++ backend/repetitor_backend/external_api/accents.py
@@ -14,7 +14,11 @@ def remove_accents(word: str) -> str:
     decomposition = unicodedata.normalize("NFKD", word)
 
     # Remove all characters that are not letters.
-    letters = "".join(c for c in decomposition if unicodedata.combining(c) == 0)
+    letters = "".join(c for c in decomposition if unicodedata.combining(c) == 0 and c not in "іїйё")
 
     # Return the word with accents removed.
     return letters
+
+if __name__ == "__main__":
+    word = "новий"
+    print(remove_accents(word))
