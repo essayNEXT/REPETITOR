94f81d12e9f3f489c01cf02fd858608f02de5618
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-08-30 22:16:18 +0300

    видалив файл з тестовими клавіатурами, хендлерами для них та прибрав їх імпорт з main.py

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -3,7 +3,6 @@ import logging
 from middlewares import first_contact
 from handlers import (
     translate,
-    inline_kb_test,
     first_contact_handler,
     customer_context_handler,
 )
@@ -46,7 +45,6 @@ async def main():
     await set_commands(bot)
     dp.include_router(first_contact_handler.router)
     dp.include_router(customer_context_handler.router)
-    dp.include_router(inline_kb_test.router)
     dp.include_router(translate.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=True)
