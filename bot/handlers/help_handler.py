6ed94a861c71ace04229b07c21476b34497a7ea2
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2023-09-01 21:14:17 +0300

    Оновив фільтри в хендлерах через фикористання F фільтрів aiogram

--- bot/handlers/help_handler.py
+++ bot/handlers/help_handler.py
@@ -1,5 +1,5 @@
-from aiogram import Router, flags
-from aiogram.filters import Text, Command
+from aiogram import Router, F
+from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from keyboards.help.help_kb import HelpKeyboard
 from aiogram.fsm.context import FSMContext
@@ -10,17 +10,28 @@ from keyboards.inline_keyboard import KeyKeyboard
 router = Router()
 
 
-@router.callback_query(Text(text="help_remove"))
+@router.callback_query(F.data == "help_remove")
 async def help_remove(callback: CallbackQuery):
     await callback.message.delete()
 
 
-@router.callback_query(Text(text="help_ok"))
-async def help_ok(callback: CallbackQuery):
-    await callback.message.delete()
+@router.callback_query(F.data == "help_thanks")
+async def help_thanks(callback: CallbackQuery, tmp_storage: TmpStorage):
+    key = KeyKeyboard(
+        bot_id=bot.id,
+        chat_id=callback.message.chat.id,
+        user_id=callback.from_user.id,
+        message_id=callback.message.message_id - 1,
+    )
+    kb = tmp_storage[key]
+
+    # Оновлюємо лічильний негативних відгуків
+    await kb.send_positive_feedback()
+
+    # await callback.message.delete()
 
 
-@router.callback_query(Text(text="help_report_problem"))
+@router.callback_query(F.data == "help_report_problem")
 async def report_about_problem(callback: CallbackQuery, tmp_storage: TmpStorage):
     """Хендлер, що ловить колбек при натисканні кнопки [повідомити про проблему]."""
     key = KeyKeyboard(
@@ -36,10 +47,8 @@ async def report_about_problem(callback: CallbackQuery, tmp_storage: TmpStorage)
     )
 
 
-@router.callback_query(Text(text="help_problem_cancel"))
-async def enter_user_translation_cancel(
-    callback: CallbackQuery, state: FSMContext, tmp_storage: TmpStorage
-):
+@router.callback_query(F.data == "help_problem_cancel")
+async def help_problem_cancel(callback: CallbackQuery, tmp_storage: TmpStorage):
     """Хендлер, що ловить колбек при натисканні кнопки [❌] (скасувати)."""
     key = KeyKeyboard(
         bot_id=bot.id,
@@ -52,8 +61,31 @@ async def enter_user_translation_cancel(
     await callback.message.edit_text(kb.text, reply_markup=kb.markup())
 
 
+@router.callback_query(F.data.func(lambda data: data.startswith("help_problem_")))
+async def help_problem_report(callback: CallbackQuery, tmp_storage: TmpStorage):
+    """Хендлер, що ловить колбек при натисканні кнопки [Проблема №_] та відправляє негативний відгук."""
+    key = KeyKeyboard(
+        bot_id=bot.id,
+        chat_id=callback.message.chat.id,
+        user_id=callback.from_user.id,
+        message_id=callback.message.message_id - 1,
+    )
+    kb = tmp_storage[key]
+
+    # Оновлюємо лічильний негативних відгуків
+    await kb.send_negative_feedback()
+    # Відправляємо опис проблеми для подальшого опрацювання
+    await kb.send_problem_report(callback.data)
+
+    await callback.message.edit_text(kb.text, reply_markup=kb.markup())
+
+
 @router.message(Command("help"))
 async def help_command(message: Message, tmp_storage: TmpStorage, state: FSMContext):
+    """
+    Хендлер приймає команду /help.
+    З БД отримуємо та надсилаємо повідомлення допомоги для поточного стану користувача.
+    """
     # Отримуємо поточний стан користувача
     user_state = await state.get_state()
 
